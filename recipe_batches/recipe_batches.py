7e1a49dc6025f2353009515fdaa94a668915a3e7
Nathaniel Mosco <natemosco@gmail.com>
2020-03-20 00:11:16 -0400

    recipe and stock completed

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,22 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    limiting_area = 10000000000000
+    for key in recipe:
+        if ingredients.get(key) is None:
+            limiting_area = 0
+            return limiting_area
+        elif ingredients[key] // recipe[key] < limiting_area:
+            limiting_area = ingredients[key] // recipe[key]
+    return limiting_area
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
